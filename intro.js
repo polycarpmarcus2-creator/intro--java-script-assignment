79131ce333b56d33e603cb30f719a6f1ab9cd5dd
polycarpmarcus2-creator <polycarpmarcus2@gmail.com>
2026-07-10 23:49:21 +0300

    Add files via upload

--- intro.js
+++ intro.js
@@ -1,3 +1,3 @@
 console.log("Name: Polycarp Magati")
-console.log("Age:25")
-console.log("Favorite Programming Language")
+console.log("Age: 20")
+console.log("Favorite Programming Language: Javascript.")
